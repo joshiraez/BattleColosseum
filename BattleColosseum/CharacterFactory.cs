f61ca7a4559b2fbc1bb6860401925e6bdf7448f0
Jose 'Joshi' Raez <joseraezrodriguez@gmail.com>
2017-07-11 12:57:37 +0200

    Redone classData using a Struct to save the data

--- BattleColosseum/CharacterFactory.cs
+++ BattleColosseum/CharacterFactory.cs
@@ -12,6 +12,6 @@ namespace BattleColosseum
         {
             
             return null;
-        }
+
     }
 }
--- BattleColosseum/ClassData.cs
+++ BattleColosseum/ClassData.cs
@@ -8,42 +8,66 @@ namespace BattleColosseum
 {
     public static class ClassData
     {
-        static byte[][] baseGrowths = {
-            new byte[] { 50, 30, 30, 20, 20, 30, 20  } //WARRIOR - 0
-        };
 
-        static byte[][] baseStats = {
-            new byte[] { 10, 0, 0, 0, 0, 0, 0  }        //WARRIOR - 0
-        };
-
-        static Weapon[] baseWeapon =
+        public static Class getRandomClass()
         {
-            Weapon.IRON_SWORD                           //WARRIOR - 0
-        };
-
-        static Ability[][] baseAbilities =
-        {
-             new Ability[] { }                          //WARRIOR - 0
-        };
+            Random rng = new Random();
+            return classData[rng.Next(classData.Length)].className;
+        }
 
         public static byte[] getBaseGrowths (this Class className)
         {
-            return baseGrowths[(int)className];
+            return classData[(int)className].baseGrowths;
         }
 
         public static byte[] getBaseStats(this Class className)
         {
-            return baseStats[(int)className];
+            return classData[(int)className].baseStats;
         }
 
         public static Weapon getBaseWeapon(this Class className)
         {
-            return baseWeapon[(int)className];
+            return classData[(int)className].baseWeapon;
         }
 
         public static Ability[] getBaseAbilities(this Class className)
         {
-            return baseAbilities[(int)className];
+            return classData[(int)className].baseAbilities;
+        }
+
+
+        //Really thinking this can be heavily improved. For now, it's okay for easy access, but in the future
+        //I should externalize the data. All the data in a static class will be loaded in RAM even if it's not used.
+        //Not desirable, when it's just a dictionary of data.
+
+        public static ClassStats[] classData =
+        {
+            new ClassStats
+                (
+                    Class.WARRIOR, // 0
+                    new byte[] { 50, 30, 30, 20, 20, 30, 20  },
+                    new byte[] { 10, 0, 0, 0, 0, 0, 0  },
+                    Weapon.IRON_SWORD,
+                    new Ability[] { }
+                )
+        };
+
+        public struct ClassStats
+        {
+            public Class className { get; }
+            public byte[] baseGrowths { get; }
+            public byte[] baseStats { get; }
+            public Weapon baseWeapon { get; }
+            public Ability[] baseAbilities { get; }
+
+            public ClassStats(Class className, byte[] baseGrowths, byte[] baseStats, Weapon baseWeapon, Ability[] baseAbilities)
+            {
+                this.className = className;
+                this.baseGrowths = baseGrowths;
+                this.baseStats = baseStats;
+                this.baseWeapon = baseWeapon;
+                this.baseAbilities = baseAbilities;
+            }
         }
     }
 }
