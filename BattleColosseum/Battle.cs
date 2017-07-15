e9e395b74e3006563d3800fbc03192c70cecc205
Jose 'Joshi' Raez <joseraezrodriguez@gmail.com>
2017-07-15 13:18:09 +0200

    Starting the Battle class, and the show interface for inversion of control.

--- BattleColosseum/Battle.cs
+++ BattleColosseum/Battle.cs
@@ -8,8 +8,19 @@ namespace BattleColosseum
 {
     public class Battle
     {
-        Character player1;
-        Character player2;
+        Character player1 { get; }
+        Character player2 { get; }
+        IBattleShow show;
+
+        public Battle(Character player1, Character player2, ShowType show)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+            this.show = BattleShowFactory(show, player1, player2);
+        }
+
+        //Another constructor with the Interface
+
 
     }
 }
---
+++ BattleColosseum/IBattleShow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleColosseum
+{
+    public interface IBattleShow
+    {
+        void showMessage();
+        void attack(AttackType attack, Battler num, byte damage);
+        //void ability(Ability ability)
+    }
+}
